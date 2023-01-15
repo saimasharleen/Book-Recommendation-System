b3c4a7f86074bf885f540dc4f688a4b4de2a4d7c
Saima Sharleen Islam <saimasharleen@users.noreply.github.com>
2023-01-15 02:12:16 +0100

    filling up the missing values

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+import pandas as pd
+from faker import Faker
+import random
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# create an instance of the faker generator
+fake = Faker()
 
+# create a list to store the generated data
+data = []
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# generate data for 80000 users
+for _ in range(80000):
+    user_id = fake.random_int(min=1, max=80000)
+    query_id = fake.random_int(min=1, max=1000)
+    rating = None if random.random() < 0.2 else fake.random_int(min=1, max=100)
+    data.append({'userId': user_id, 'queryId': query_id, 'rating': rating})
 
+# convert the list to a dataframe
+df = pd.DataFrame(data)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# save the dataframe to a csv file
+df.to_csv("fake_data_missing_ratings.csv", index=False)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
